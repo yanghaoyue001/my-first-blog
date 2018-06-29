bd7faa9e752e6793496cbd495faff24f679d03e5
haoyueyang <yanghaoyue001@gmail.com>
2018-06-28 17:38:32 -0700

    added views to create/edit blog post inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns=[
   #  path('^$',views.post_list,name='post_list'),
     path('',views.post_list,name='post_list'),
-    path('^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
